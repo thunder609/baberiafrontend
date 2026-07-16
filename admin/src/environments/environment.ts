7ccb344137f00b6af46e092b6d1d224493aaf4b0
thunder609 <oswaldo-pacheco@hotmail.es>
2026-07-16 00:25:04 -0400

    enviroment prod

--- admin/src/environments/environment.ts
+++ admin/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://barbariabackend-1-0.onrender.com',
   supabaseUrl: 'https://etnmiqruanmqyiejzrlc.supabase.co', // Reemplazá con tu Project URL
   supabaseKey: 'sb_publishable_8UpLHQt78HJnHmwgCPcSHw_7jHIl0T9' // Reemplazá con tu Public Key
 
